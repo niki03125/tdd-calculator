9dc02115d817b0b1b623225f4702d7ea508660e0
niki03125 <Nikita.po@hotmail.com>
2025-02-02 11:06:26 +0100

    chanches to add() overload

--- Week-1-Friday-Exercise/src/main/java/tdd/Calculator.java
+++ Week-1-Friday-Exercise/src/main/java/tdd/Calculator.java
@@ -17,4 +17,22 @@ public class Calculator {
     public int divide(int a, int b) {
         return a / b;
     }
+
+    public int add (String numbers){
+        String[] numberArray = numbers.split(",");
+        int sum = 0;
+        for(String num: numberArray){
+            sum += Integer.parseInt(num);
+        }
+        return sum;
+    }
+
+    public int add(int[] numbers){
+        int sum = 0;
+        for(int num : numbers){
+            sum += num;
+        }
+        return sum;
+    }
+
 }
